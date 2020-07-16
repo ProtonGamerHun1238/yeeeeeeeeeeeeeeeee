0e819c7a9ecf17e9800196e85fca8e3c307cdd0e
ProtonGamerHun1238 <68368558+ProtonGamerHun1238@users.noreply.github.com>
2020-07-16 09:46:37 +0200

    yeeeeeeeeeeeeeeeee

--- main.ts
+++ main.ts
@@ -1,3 +1,14 @@
+let kpo = 0
+let pontok = 0
+input.onButtonPressed(Button.A, function () {
+    kpo = randint(1, 3)
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showNumber(pontok)
+})
+input.onButtonPressed(Button.B, function () {
+    pontok += 1
+})
 basic.forever(function () {
 	
 })
