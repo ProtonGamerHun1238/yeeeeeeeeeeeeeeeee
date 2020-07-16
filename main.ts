d4dd9777c1d9113ba722688d6eea2ed671fdae20
ProtonGamerHun1238 <68368558+ProtonGamerHun1238@users.noreply.github.com>
2020-07-16 09:56:12 +0200

    push

--- main.ts
+++ main.ts
@@ -1,10 +1,35 @@
-let kpo = 0
 let pontok = 0
+let kpo = 0
 input.onButtonPressed(Button.A, function () {
-    kpo = randint(1, 3)
-})
-input.onButtonPressed(Button.AB, function () {
     basic.showNumber(pontok)
+})
+input.onGesture(Gesture.Shake, function () {
+    kpo = randint(0, 2)
+    if (kpo == 0) {
+        basic.showLeds(`
+            . . . . .
+            . # # # .
+            . # # # .
+            . # # # .
+            . . . . .
+            `)
+    } else if (kpo == 1) {
+        basic.showLeds(`
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            `)
+    } else if (kpo == 2) {
+        basic.showLeds(`
+            # . . . #
+            . # . # .
+            . . # . .
+            # # . # #
+            # # . # #
+            `)
+    }
 })
 input.onButtonPressed(Button.B, function () {
     pontok += 1
